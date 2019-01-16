bb4b8757c17a524d5f675ccf31ed2e7be41af837
AmirAli <3abd2000@gmail.com>
2019-01-16 09:21:43 +0330

    Update random.cpp

--- MineSweeper2/random.cpp
+++ MineSweeper2/random.cpp
@@ -1,3 +1,20 @@
+void find_number (int a[10][10])
+{
+    int i,j,c;
+    for(i=0;i<10;i++)
+    {
+        for(j=0;j<10;j++)
+        {
+            if(a[i][j]!=9)
+            {
+            c=0;
+            if(a[i+1][j+1]==9)
+                c++;
+            if(a[i][j+1]==9)
+                c++;
+                if(a[i+1][j]==9)
+                c++;
+                if(i-1>=0)@@ -0,0 +1,38 @@
 #include <iostream>
 using namespace std;
 #include <stdlib.h>
@@ -6,19 +23,19 @@ using namespace std;
 const int n=30;
 const int m=100;
 
-void printArr (char a[n][n]) {
+void printArr (char map[n][n]) {
     for (int i=0; i<n; i++){
         for (int j=0; j<n; j++){
-            cout<<a[i][j]<<' ';
+            cout<<map[i][j]<<' ';
         }
         cout<<endl;
     }
 }
-void bombRandom (char a[n][n]) {
+void bombRandom (char map[n][n]) {
 int i, j;
 for (i=0; i<n; i++){
     for (j=0; j<n; j++){
-        a[i][j]='0';
+        map[i][j]='0';
     }
 }
 
@@ -26,13 +43,13 @@ srand(time(NULL));
 for (int t=0; t<m; t++){
     i = rand() % n;
     j = rand() % n;
-    a[i][j]='9';
+    map[i][j]='9';
 }
 }
 
 int main () {
-    char a[n][n];
-    bombRandom (a);
-    printArr (a);
+    char map[n][n];
+    bombRandom (map);
+    printArr (map);
 
 }

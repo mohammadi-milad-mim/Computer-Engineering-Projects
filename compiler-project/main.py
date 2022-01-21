afc165d60b4f6205647ff284016d4919900e4694
MilladMuhammadi <43762761+MilladMuhammadi@users.noreply.github.com>
2022-01-21 21:24:14 +0330

    Update main.py

--- compiler-project/main.py
+++ compiler-project/main.py
@@ -1,5 +1,79 @@
-# 1
+import re
 
+def comment_remover(text):
+    def replacer(match):
+        s = match.group(0)
+        if s.startswith('/'):
+            return " " # note: a space and not an empty string
+        else:
+            return s
+    pattern = re.compile(
+        r'//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'|"(?:\\.|[^\\"])*"',
+        re.DOTALL | re.MULTILINE
+    )
+    return re.sub(pattern, replacer, text)
+
+number_of_files = 1
+for i in range(number_of_files):
+    in_file_name = "files_0/f_0_{num}.txt".format(num=i+1)
+    out_file_name = "files_1/f_1_{num}.txt".format(num=i+1)
+    temp_file_name = "files_0/t_0_{num}.txt".format(num=i+1)
+    fin = open(in_file_name, "r")
+    fout = open(out_file_name, "w")
+    ftemp = open(temp_file_name,"w")
+    temp = ""
+    define_list = {}
+    #Find include, paste them on ouput file
+    for line in fin.readlines():
+        sline = line.split()
+        if re.search("#\s*include", line):
+            path = "files_0/"+sline[-1][1:-1]
+            tt = open(path, "r")
+            temp+=tt.read()
+            temp+='\n'
+            #print(temp)
+        else:
+            temp+=line
+    ftemp.write(temp)
+    ftemp.close()
+    
+    ftemp = open(temp_file_name,"r")
+    temp = ""
+    for line in ftemp.readlines():
+        #print(line)
+        sline = line.split()
+        if re.search("#\s*define", line):
+            idx=0
+            for i in range(len(sline)):
+                if i=="#define" or i=="define":
+                    idx=i+1
+            tkey = sline[idx+1]
+            tvalue = ' '.join(sline[idx+2:])
+            #print(tkey)
+            #print(tvalue)
+            define_list[tkey]=tvalue
+        else:
+            temp+=line
+    temp = comment_remover(temp)
+    print(temp)
+    print(define_list)
+    for key, value in define_list.items():
+        print(key,value)
+        temp = temp.replace(key,value)
+    
+    temp = comment_remover(temp)
+    fout.write(temp)
+    fout.close()
+
+
+
+
+#The OLD Version. Fixed Bugs!
+
+
+
+
+'''
 my_file = open("preprocessed.txt", "w")
 file = open("input.cpp", "r")
 
@@ -63,3 +137,4 @@ while True:
 
 
 # ----- typedefs -----
+'''

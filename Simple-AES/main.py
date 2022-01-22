4d688af78fcf05843210c5240bf9e553aa908488
MilladMuhammadi <43762761+MilladMuhammadi@users.noreply.github.com>
2022-01-22 21:31:33 +0330

    Update main.py

--- Simple-AES/main.py
+++ Simple-AES/main.py
@@ -13,19 +13,13 @@ cipher_parts = []
 for i in plain_parts:
     cipher_parts.append(aes_e(i,round_keys))
     
-#print(cipher_parts)
+#write cipher parts on cipher.txt  
 #write_to_file_cipher(cipher_parts)
 
 output_parts = []
 for i in cipher_parts:
-    tem = aes_d(i,round_keys)
-    output_parts.append(reArrangeOutPut(tem))
-#print(output_parts)
-output_parts[-1]=output_parts[-1][:16-len_padd]
+    output_parts.append(reArrangeOutPut(aes_d(i,round_keys)))
 
+
+output_parts = removePadding(output_parts)
 write_to_file("files/output.txt",output_parts)
-#write cipher parts on cipher.txt
-
-#chunkcipherparts
-
-#do aes decrypt on each cipherparts and put it on output
